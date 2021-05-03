2f167a90ed962b6f426879084824deaafae2f2e2
edgarZemedu <edgar.es.et@gmail.com>
2021-05-03 18:59:00 +0200

    add AD_examen_2ºEvR

--- Estadistica/app/src/main/java/com/example/estadistica/MainActivity.java
+++ Estadistica/app/src/main/java/com/example/estadistica/MainActivity.java
@@ -9,12 +9,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et1;
     TextView tv;
-    ArrayList<Integer> a;
+    List<Integer> a;
     int uNumero;
 
 
@@ -26,24 +29,13 @@ public class MainActivity extends AppCompatActivity {
         tv = findViewById(R.id.tv);
         a = new ArrayList<Integer>();
         uNumero = Integer.parseInt(String.valueOf(et1.getText()));
-        //while(uNumero == 37){
-            if (et1.getText() != null){
-                Toast.makeText(this," ** bien ->  "+uNumero,Toast.LENGTH_SHORT).show();
-            }
-            a.add(uNumero);
-            Toast.makeText(this,"  ******->  "+uNumero,Toast.LENGTH_SHORT).show();
-        //}
+        a.add(uNumero);
+        Toast.makeText(this,"  ******->  "+uNumero,Toast.LENGTH_SHORT).show();
+
     }
 
     public void boton(View view) {
-        if (!a.isEmpty()){
-            for (int i = 0; i < a.size(); i++){
-                tv.setText(a.get(i));
-                Toast.makeText(this,"deberia mostrarse",Toast.LENGTH_SHORT).show();
-            }
-        }else{
-            Toast.makeText(this,"Arraylist vacio",Toast.LENGTH_SHORT).show();
-        }
+
 
     }
 }
