78b7c9d11e9a4ef5ddfd9635f97bea49b5dbae06
edgarZemedu <edgar.es.et@gmail.com>
2021-05-23 17:34:59 +0200

    ..

---
+++ PMDM/java/com/zeme/examen2ev_pdmd_z/RecyclerView.java
@@ -0,0 +1,19 @@
+package com.zeme.examen2ev_pdmd_z;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecyclerView extends AppCompatActivity {
+
+    RecyclerView rv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        
+
+    }
+
+}
